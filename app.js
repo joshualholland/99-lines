13986fa16ab93f62b30a3feb2c8955b201decf07
joshualholland <joshualholland@gmail.com>
2018-10-09 16:24:58 -0500

    jk finished now

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ for (i = 0; i < friends.length; i++) {
         for (l = 99; l > 0; l--) {
             var minusOne = l - 1;
             if (l > 1) {
-                console.log(l + "lines of code in the file," + l + "lines of code;" + friend + "strikes one out," + "clears it all out," + l + "lines of code in the file")
+                console.log(l + "lines of code in the file," + l + "lines of code;" + friend + "strikes one out," + "clears it all out," + minusOne + "lines of code in the file")
             } else {
                 console.log(l + "lines of code in the file," + l + "lines of code" + friend + "strikes one out," + "clears it all out," + "no more lines of code in the file.")
             }
